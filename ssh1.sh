9c029ae5ac160738f9e8aa03fc5dbcd5b72bd857
Sukeharu Kano <sukeharu@solidpanda.com>
2018-11-09 00:00:01 +0900

    Update ssh1.sh

--- ssh1.sh
+++ ssh1.sh
@@ -1,4 +1,4 @@
 $ mkdir ~/ssh
 $ chmod 600 ~/ssh
-$ cp ~/Downloads/<id>.key ~/ssh/<id>_rsa # ~/Downloads...はダウンロードしたファイルのパスとファイル名。コピー先ファイル名の<id>_rsaは何でもよいが、半角。拡張子なし
+$ cp ~/Downloads/<id>.key ~/ssh/<id>_rsa # ~/Downloads...はダウンロードしたファイルのパスとファイル名。コピー先ファイル名のid_<id>_rsaは何でもよいが、半角。拡張子なし
 $ chmod 400 <id>_rsa
