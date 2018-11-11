ddd37874c04787f0dbcc0a9ac4bda8178c9b0b78
Sukeharu Kano <sukeharu@solidpanda.com>
2018-11-11 21:17:36 +0900

    bash script for setting up wp-cli

--- wordpress/wpcli-atxserver.sh
+++ wordpress/wpcli-atxserver.sh
@@ -4,4 +4,4 @@ $ cd bin
 $ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 $ chmod +x wp-cli.phar
 $ echo "alias wp='/opt/php-7.2.6/bin/php ~/bin/wp-cli.phar'" >> ~/.bashrc
-$ source source ~/.bashrc
+$ source ~/.bashrc
---
+++ wordpress/wpcli-atxserver3.sh
@@ -0,0 +1,3 @@
+$ cd
+$ echo 'source /home/<account_id>/bin/wp-completion.bash' >> .bash_profile
+$ source .bash_profile
