ecdbba38f101038983db6cd59a23b402c6e2929e
Sukeharu Kano <sukeharu@solidpanda.com>
2018-11-11 18:57:29 +0900

    Update wordpress/wpcli-atxserver.sh

--- wordpress/wpcli-atxserver.sh
+++ wordpress/wpcli-atxserver.sh
@@ -3,5 +3,5 @@ $ mkdir bin
 $ cd bin
 $ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 $ chmod +x wp-cli.phar
-$ alias 'wp=/opt/php-7.2.6/bin/php ~/bin/wp-cli.phar'
+$ echo "alias wp='/opt/php-7.2.6/bin/php ~/bin/wp-cli.phar'" >> ~/.bashrc
 $ source source ~/.bashrc
